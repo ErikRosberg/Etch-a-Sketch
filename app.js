e3d3bd4ae68e41703ae341843270f543ed929ac1
ErikRosberg00 <Erik.Rosberg1@gmail.com>
2022-12-12 11:32:31 +0100

    started new branch for remaking the app following instructions.
    I've fixed basic styling and created a function that creates a 16x16 grid

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
-// const drawingBox = getElementById('drawingBox');
-// const brushSize = changebrushSize();
-// const brushColor = changeColor();
-
-const brushColor = "red";
-const brushSize = "12px";
-
-// const drawingBox = document.createElement('div');   
-// drawingBox.setAttribute('style', 'width: 2rem; height: 2rem; background: white; border: 2px solid black;');    
-// document.getElementById("drawingArea").appendChild(drawingBox);
+const printArea = document.getElementById("drawingGrid")
 
 
+function createGrid(rows, cols) {
+    printArea.style.setProperty('--grid-rows', rows);
+    printArea.style.setProperty('--grid-cols', cols);  
 
-function createDrawingArea() {
-    let i = 1;
-    while(i < 307) {
-        console.log('hello Drawing - Area!');
-        const drawingBox = document.createElement('div');
-        drawingBox.classList.add('drawing-box-:'+i);   
-        drawingBox.setAttribute('style', 'width: 2rem; height: 2rem; background: white; border: 1px solid black; margin:0; padding:0;');    
-        document.getElementById("drawingArea").appendChild(drawingBox);
-        i++
+    for(i = 0; i < (rows*cols); i++) {
+        let cell = document.createElement('div')
+        printArea.appendChild(cell).className = "grid-item";
+
     }
 }
 
-function changeColor(brushColor) {
-    let color = brushColor;
-    console.log('hello Brush - Color!');
-    
-    return color;
-}
+document.onload = createGrid(16, 16);
 
-function changebrushSize(brushSize) {
-    let size = brushSize;
-    console.log('hello Brush - Size!');
-    return size;
-}
-
-function clearDrawingBoard() {
-        createDrawingArea();        
-}
